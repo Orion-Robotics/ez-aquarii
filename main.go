b6d500915d5c9ce0b881f97968d5b1a5bc1ce188
Niklas Austermann <niklaustermann@gmail.com>
2021-09-08 15:27:54 -0400

    add protobuf codegen

--- main.go
+++ main.go
@@ -1,4 +1,14 @@
 package main
 
+import "github.com/team-orion/ez-aquarii/gen"
+
 func main() {
+	packet := gen.Packet{
+		Time: time.Now(),
+	}
+	bin, err := proto.Marshal(packet)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(bin)
 }
