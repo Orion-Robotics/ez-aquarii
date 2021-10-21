e09e12cd731841f6b64d610325ab8d9abf105e3d
Bluskript <bluskript@gmail.com>
2021-10-21 16:32:09 -0400

    working platformio project with LTC2496

---
+++ reader/src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#include <LTC2496/LTC2496.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
