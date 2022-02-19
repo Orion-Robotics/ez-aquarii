8c8dc839c74619bc644aed6342b4f6aad16e2e08
Bluskript <bluskript@gmail.com>
2022-02-19 01:22:42 -0500

    add ssh tunnel command

--- pibot/pibot.py
+++ pibot/pibot.py
@@ -4,6 +4,7 @@ import urllib.request
 
 from discord.ext import commands
 from dotenv import load_dotenv
+from pyngrok import ngrok
 
 load_dotenv()
 
@@ -15,6 +16,12 @@ async def get_ip(ctx):
     local_ip = socket.gethostbyname(socket.gethostname())
     external_ip = urllib.request.urlopen("https://ident.me").read().decode("utf8")
     await ctx.send(f"Local IP: {local_ip}\nExternal IP: {external_ip}")
+
+
+@bot.command()
+async def start_ngrok(ctx):
+    ssh_tunnel = ngrok.connect(22, "tcp")
+    await ctx.send(f"SSH tunnel started: {ssh_tunnel}")
 
 
 bot.run(os.getenv("DISCORD_TOKEN"))
