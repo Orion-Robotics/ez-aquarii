0fc9a4e59d234aca31cabc4e4c2f85a62e822ce3
Bluskript <bluskript@gmail.com>
2022-02-19 03:37:11 -0500

    add a way to start, stop, and inspect journals
    adjust journal truncation limit
    
    add command to update from git
    
    rename ngrok command
    
    subprocess.run not subprocess.call
    
    the argument order was messed up
    
    dont join everything with newline wtf
    
    last 500 characters not everything before last 500
    
    remove all the noisy systemd logging stuff
    
    make external ip not critical information
    
    getfqdn to get the actual local ip
    
    ffs just give a whole list of IPs
    
    ok actually just do a little trolling to get the ip address

--- pibot/pibot.py
+++ pibot/pibot.py
@@ -1,30 +1,89 @@
 import os
 import socket
+import subprocess
 import urllib.request
 
 from discord.ext import commands
 from dotenv import load_dotenv
 from pyngrok import ngrok
 
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+
 load_dotenv()
 
 bot = commands.Bot(command_prefix="$")
 
 
-@bot.command()
+@bot.command(description="Get the local and public IP of the pi")
 async def get_ip(ctx):
-    local_ip = socket.gethostbyname(socket.gethostname())
-    external_ip = urllib.request.urlopen("https://ident.me").read().decode("utf8")
+    local_ip = get_ip_address()
+    external_ip = None
+    try:
+        external_ip = urllib.request.urlopen("https://ident.me").read().decode("utf8")
+    except:
+        pass
     await ctx.send(f"Local IP: {local_ip}\nExternal IP: {external_ip}")
 
 
-@bot.command()
+@bot.command(name="ngrok", description="Starts an ngrok tunnel to the pi")
 async def start_ngrok(ctx):
     ssh_tunnel = ngrok.connect(22, "tcp")
     host, _, port = ssh_tunnel.public_url[6:].partition(":")
     await ctx.send(
         f"SSH tunnel started: {ssh_tunnel.public_url} (`ssh pi@{host} -p {port}`)"
     )
+
+
+@bot.command(description="Updates from git")
+async def update(ctx):
+    path = os.getenv("GIT_REPO_PATH") or "."
+    user = os.getenv("GIT_USER") or ""
+    password = os.getenv("GIT_PASSWORD") or ""
+    subprocess.run(
+        [
+            "git",
+            "-C",
+            path,
+            "pull",
+            f"https://{user}:{password}@gitlab.com/princeton-soccer-robotics/ez-aquarii.git",
+        ]
+    )
+    await ctx.send("Updated!")
+
+
+@bot.command(description="Restarts a user service")
+async def restart(ctx, service: str):
+    subprocess.run(["systemctl", "--user", "restart", service])
+
+
+@bot.command(description="Starts a user service")
+async def start(ctx, service: str):
+    subprocess.run(["systemctl", "--user", "start", service])
+
+
+@bot.command(description="Stops a user service")
+async def stop(ctx, service: str):
+    subprocess.run(["systemctl", "--user", "stop", service])
+
+
+@bot.command(description="Reads the journal")
+async def journal(ctx, service: str):
+    try:
+        result = subprocess.run(
+            ["journalctl", "--user", "-xeu", service, "--no-pager", "--output", "cat"],
+            capture_output=True,
+        ).stdout.decode("utf8")
+
+        truncated = result[-500:] if len(result) > 500 else result
+
+        await ctx.send(f"```\n{truncated}\n```")
+    except Exception as e:
+        await ctx.send(f"Error reading journal data {e}")
 
 
 bot.run(os.getenv("DISCORD_TOKEN"))
