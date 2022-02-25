a0ca89b6a58a6722554f8d0eccb642ae2425e52d
Pythomancer <austermannniklas@outlook.com>
2022-02-25 13:51:17 -0500

    added ball selector, which is ineffective

--- camera/experiments/filter.py
+++ camera/experiments/filter.py
@@ -1,21 +1,12 @@
 import numpy as np
 import cv2
 
-HDIFF = 35
-SDIFF = 35
-VDIFF = 80
-# params = cv2.SimpleBlobDetector_Params()
-# params.minThreshold = 10
-# params.maxThreshold = 200
-# params.filterByArea = False
-# params.filterByCircularity = True
-# params.minCircularity = 0.3
-# params.filterByInertia = True
-# params.minInertiaRatio = 0.01
+HDIFF = 20
+SDIFF = 40
+VDIFF = 60
 
-# detector = cv2.SimpleBlobDetector_create(params)
 frame = cv2.imread("image.jpg")
-hsv = frame  # cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+hsv = frame
 lower_blue = np.array([60, 35, 140])
 upper_blue = np.array([180, 255, 255])
 mask = cv2.inRange(hsv, lower_blue, upper_blue)
@@ -33,7 +24,11 @@ def click(event, x, y, flags, param):
         mask = cv2.GaussianBlur(mask, (5, 5), 0)
         result = cv2.bitwise_and(frame, frame, mask=mask)
         contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.drawContours(result, contours, -1, (0, 255, 0), 1)
+        m = 0
+        for i in range(1, len(contours)):
+            if len(contours[i]) > len(contours[m]):
+                m = 1
+        cv2.drawContours(result, contours, m, (0, 255, 0), 1)
         two = np.concatenate((result, frame), axis=1)
         cv2.imshow("frame", two)
 
