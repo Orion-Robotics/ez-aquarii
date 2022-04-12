f5e3b84c9934b9c765d878b8b82aefc4656cfd6d
uooh <niklaustermann@gmail.com>
2022-04-12 19:04:32 -0400

    fix pi-side reloading

--- pi_dev.sh
+++ pi_dev.sh
@@ -7,11 +7,20 @@ if [ "$COMPONENTS" == "camera" ]; then
   jurigged ./camera &
 fi
 if [ "$COMPONENTS" == "controller" ]; then
-  inotifywait -rqme modify,create,delete,move ./controller/target/controller |
-  while read -r filename event; do
-    tput setaf 3
-    echo "reloading..."
-    tput sgr0
-    ./controller/target/controller
+  echo "started controller, listening for changes..."
+  ./controller/target/controller &
+  CONTROLLER_PID=$!
+  inotifywait -qme modify,create,delete,move ./controller/target |
+  while read -r dir event file; do
+    if [ "$file" = "controller" ]; then
+      if [ -z "$CONTROLLER_PID" ]; then
+        kill -9 CONTROLLER_PID
+      fi
+      tput setaf 3
+      echo "reloading..."
+      tput sgr0
+      ./controller/target/controller &
+      CONTROLLER_PID=$!
+    fi
   done
 fi
