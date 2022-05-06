8afe29aa7fdb8006fe0c5f01337470a94020842b
Pythomancer <austermannniklas@outlook.com>
2022-05-06 15:17:53 -0400

    camera thresholding for three different objects

--- camera/filter.py
+++ camera/filter.py
@@ -10,59 +10,80 @@ from handlers.display import DisplayHandler
 from handlers.noop import NoopHandler
 from lib.streaming import StreamingFrameHandler
 
+thresholds = [
+    [255, 0, 255, 0, 255, 0],
+    [255, 0, 255, 0, 255, 0],
+    [255, 0, 255, 0, 255, 0],
+]
+names = ["HH", "HL", "VH", "VL", "SH", "SL"]
+
+# ball, goal1, goal2
+current = 0
+
 
 def HH(val):
-    thresholds[0] = val
+    thresholds[current][0] = val
     refresh()
 
 
 def HL(val):
-    thresholds[1] = val
+    thresholds[current][1] = val
     refresh()
 
 
 def SH(val):
-    thresholds[2] = val
+    thresholds[current][2] = val
     refresh()
 
 
 def SL(val):
-    thresholds[3] = val
+    thresholds[current][3] = val
     refresh()
 
 
 def VH(val):
-    thresholds[4] = val
+    thresholds[current][4] = val
     refresh()
 
 
 def VL(val):
-    thresholds[5] = val
+    thresholds[current][5] = val
     refresh()
 
 
+fns = [HH, HL, VH, VL, SH, SL]
+
+
 def refresh():
-    m = mask(hsv, thresholds)
-    b = find_blob(hsv, thresholds)
+    m = mask(hsv, thresholds[current])
+    b = find_blob(hsv, thresholds[current])
     draw(m, b, center=(300, 300))
     hsvshow("mask", m)
     print(loc(b, center=(300, 300)))
+
+
+def refreshslider():
+    for i in range(6):
+        cv2.setTrackbarPos(names[i], "joe", thresholds[current][i])
 
 
 def hsvshow(name, im):
     cv2.imshow(name, cv2.cvtColor(im, cv2.COLOR_HSV2BGR))
 
 
-thresholds = [255, 0, 255, 0, 255, 0]
+def CE(val):
+    global current
+    current = val
+    refreshslider()
+
+
 joe = cv2.imread("cha.jpg")
 joe = cv2.resize(joe, (600, 600))
 hsv = cv2.cvtColor(joe, cv2.COLOR_BGR2HSV)
 hsvshow("joe", hsv)
-cv2.createTrackbar("HH", "joe", 0, 255, HH)
-cv2.createTrackbar("HL", "joe", 0, 255, HL)
-cv2.createTrackbar("VH", "joe", 0, 255, VH)
-cv2.createTrackbar("VL", "joe", 0, 255, VL)
-cv2.createTrackbar("SH", "joe", 0, 255, SH)
-cv2.createTrackbar("SL", "joe", 0, 255, SL)
+for i in range(6):
+    cv2.createTrackbar(names[i], "joe", 0, 255, fns[i])
+cv2.createTrackbar("current", "joe", 0, 2, CE)
+refreshslider()
 cv2.waitKey(0)
 cv2.destroyAllWindows()
