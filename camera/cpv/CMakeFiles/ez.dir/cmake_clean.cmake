5fe0bd019fa2f0eb3c39c564fd887fd842415b97
cad master <niklaustermann@gmail.com>
2022-06-04 14:34:21 -0400

    add cpp vision

---
+++ camera/cpv/CMakeFiles/ez.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ez.dir/main.cpp.o"
+  "CMakeFiles/ez.dir/main.cpp.o.d"
+  "ez"
+  "ez.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ez.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
