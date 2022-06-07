ea13235d689bff204d726b42ac8a43562c73b36f
CADMASTER727 <niklaustermann@gmail.com>
2022-06-06 22:27:51 -0400

    sockets??

--- ffitest/rffi/target/debug/rffi.d
+++
@@ -1,1 +0,0 @@
-/home/pythomancer/Documents/ez-aquarii/ffitest/rffi/target/debug/rffi: /home/pythomancer/Documents/ez-aquarii/ffitest/rffi/src/main.rs
--- ffitest/rffi/target/rls/debug/deps/rsock-6f330ee280ef1859.d
+++
@@ -1,5 +0,0 @@
-/home/pythomancer/Documents/ez-aquarii/sockettest/rsock/target/rls/debug/deps/rsock-6f330ee280ef1859.rmeta: src/main.rs
-
-/home/pythomancer/Documents/ez-aquarii/sockettest/rsock/target/rls/debug/deps/rsock-6f330ee280ef1859.d: src/main.rs
-
-src/main.rs:
