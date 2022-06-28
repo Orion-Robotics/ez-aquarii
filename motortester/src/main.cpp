5dfcce3aeda4e50a8efb6cdaab7c66c5922014bf
pythomancer <niklaustermann@gmail.com>
2022-06-28 14:28:21 -0400

    make reader commands work again with new drivers

--- motortester/src/main.cpp
+++ motortester/src/main.cpp
@@ -16,13 +16,13 @@ void setup() {
 void loop() {
   // Serial.println("sus");
   // for (int i = 0; i < 255; i++){
-    analogWrite(9, 127); //FR
+    analogWrite(9, 255); //FR
     analogWrite(23, 255);
-    analogWrite(22, 127); //FL
+    analogWrite(22, 255); //FL
     analogWrite(20, 0);
-    analogWrite(17, 127); //BL
+    analogWrite(17, 255); //BL
     analogWrite(16, 255);
-    analogWrite(3, 127); //BR
+    analogWrite(3, 255); //BR
     analogWrite(4, 0);
   //   delay(10);
   //   Serial.println(i);
